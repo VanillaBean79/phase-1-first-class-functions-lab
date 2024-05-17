e64ac66cab91d9131f27647f34985e37e059f6b7
Ruben Salazar <rubensalazar@rocketmail.com>
2024-05-17 17:27:18 -0500

    functions lab Phase 1

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// Code your solution in this file!
+
+const drivers = ['Antonia','Nuru','Amari','Mo']
+
+const returnFirstTwoDrivers = function(drivers){
+    return drivers.slice(0,2)
+}
+
+const drivers2 = ['Antonia','Nuru','Amari','Mo']
+
+const returnLastTwoDrivers = function(drivers2){
+    return drivers2.slice(2,4)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, 
+    returnLastTwoDrivers
+ ]
+
+ const createFareMultiplier = function(fareMultiplier){
+    return function(fare){
+        return(fare * 5)
+    }
+ }
+ 
+const fareDoubler = function(fD){
+return(fD * 2)
+}
+const fareTripler = function(fT){
+    return(fT * 3)
+}
+
+const selectDifferentDrivers = function(drivers, func) {  
+    return func(drivers)
+}
+
+
+
+     
+
+ 
+
